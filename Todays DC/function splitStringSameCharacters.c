9ffc3e98bfcc3c401e2d4a7da3b7a2b95daa5de7
RexMilton <rexmilton5@gmail.com>
2022-02-15 20:27:54 +0530

    m

--- Todays DC/function splitStringSameCharacters.c
+++ Todays DC/function splitStringSameCharacters.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-void splitStringSameCharacters(char *s)
-{
-    printf("%c", s[0]);
-    if (s[0] == s[1] && s[0] != s[2])
-        printf(" ");
-    for (int i = 1; s[i] != '\0'; i++)
-    {
-        printf("%c", s[i]);
-        if (s[i] == s[i + 1] && s[i] != s[i + 2] && s[i] != s[i - 1])
-            printf(" ");
-    }
-}
 int main()
 {
     char str[1001];
