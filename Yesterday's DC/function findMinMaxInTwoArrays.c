043a72ba805f5bc7f0eb58cae0a1f916941c78ed
RexMilton <rexmilton5@gmail.com>
2022-02-27 19:37:56 +0530

    m

--- Yesterday's DC/function findMinMaxInTwoArrays.c
+++ Yesterday's DC/function findMinMaxInTwoArrays.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+int findMinMaxInTwoArrays(int l1, int a1[], int l2, int a2[], int k)
+{
+    int x = 0, y = 0;
+    for (int i = 0; i < l1; i++)
+        if (a1[i] > k)
+            x++;
+    for (int i = 0; i < l2; i++)
+        if (a2[i] < k)
+            y++;
+    return x > y ? x : y;
+}
+
 int main()
 {
     int M, N, K;
