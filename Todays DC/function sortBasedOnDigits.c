6a85298601bd4c90d37dcaa8036f9ef708c384f7
RexMilton <rexmilton5@gmail.com>
2022-04-06 07:12:22 +0530

    m

--- Todays DC/function sortBasedOnDigits.c
+++ Todays DC/function sortBasedOnDigits.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+int fun(int x,int y){
+    char a[1000],b[1000];
+    sprintf(a,"%d",x);
+    sprintf(b,"%d",y);
+    return strcmp(a,b)>0;
+}
+void sortBasedOnDigits(int SIZE, int *arr)
+{
+    for(int i=0;i<SIZE;i++){
+        for(int j=i+1;j<SIZE;j++){
+            if(fun(arr[i],arr[j])){
+                int t=arr[i];
+                arr[i]=arr[j];
+                arr[j]=t;
+            }
+        }
+    }
+}
 int main()
 {
     int N;
