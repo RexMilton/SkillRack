7dc27da3b4a91e8495dfb1463e6e5ee3d6eeb682
RexMilton <rexmilton5@gmail.com>
2022-04-11 00:14:53 +0530

    m

--- Todays DC/function areConsecutive.c
+++ Todays DC/function areConsecutive.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+int areConsecutive(char *str)
+{
+    int ctr=strlen(str);
+    for(int i=1;str[i]!='\0';i++){
+        char a[1001]={'\0'},b[1001]={'\0'};
+        for(int j=0;j<i;j++) a[j]=str[j];
+        for(int j=i;str[j]!='\0';j++) b[j-i]=str[j];
+        if(b[0]=='0') continue;
+        int x=atoi(a),y=atoi(b);
+        if(abs(x-y)==1) return 1;
+    }
+    return 0;
+}
 int main()
 {
     char str[101];
